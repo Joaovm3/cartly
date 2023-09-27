4b8ad99f3a3f8f794548778352bcb61bc7d60361
gabrielvbauer <gabriel.bauer9@hotmail.com>
2023-09-26 21:15:43 -0300

    feat: added tailwindcss and babel module resolver

---
+++ src/screens/Home.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export function Home() {
+  return (
+    <View className="h-screen bg-zinc-900 items-center justify-center">
+      <Text className="text-zinc-100">Oi</Text>
+    </View>
+  );
+}
