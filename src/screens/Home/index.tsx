8bc2754ca72cc05b7314591502f27a0c69753fc4
gabrielvbauer <gabriel.bauer9@hotmail.com>
2023-09-30 12:31:30 -0300

    feat: DeliveryStateCard component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -14,6 +14,7 @@ import Feather from "@expo/vector-icons/Feather";
 import { Header } from "@components/Header";
 import PromotionImage from "@assets/promotion.png";
 import { SectionHeader } from "@components/SectionHeader";
+import { DeliveryStateCard } from "@components/DeliveryStateCard";
 
 const promos = [1, 2];
 const mainCategories = [
@@ -35,30 +36,7 @@ export function Home() {
         </View>
 
         <View className="px-6 mt-8">
-          <View className="p-4 bg-gray-200 rounded-md space-y-4">
-            <View className="flex-row items-center gap-4">
-              <Feather name="archive" size={28} color="#4CB944" />
-              <View>
-                <Text className="font-semibold text-green-500 text-base leading-relaxed">
-                  Coletando itens
-                </Text>
-                <Text className="text-gray-600 text-sm leading-4">
-                  Seus itens estão sendo coletados pelos entregadores
-                </Text>
-              </View>
-            </View>
-
-            <View className="w-full h-2 bg-green-500"></View>
-
-            <View className="flex-row justify-between items-center">
-              <Text className="text-gray-600 text-sm leading-4">
-                Entrega estimada
-              </Text>
-              <Text className="font-bold text-green-500 text-xl leading-relaxed">
-                17:48h
-              </Text>
-            </View>
-          </View>
+          <DeliveryStateCard />
         </View>
 
         <View className="px-6 mt-8">
